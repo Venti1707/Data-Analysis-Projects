7a2a7700615408c1cc5419cee0d7323a74c900bf
Venti1707 <nickohse3@gmail.com>
2025-05-16 12:47:27 +0800

    Create Index.py

---
+++ Social/PA Course Participation (2014 - 2016)/Index.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+import requests
+import pandas as pd
+
+dataset_id = "d_01ad15c4d10ca87328f6af80b2ffbe82"
+url = "https://data.gov.sg/api/action/datastore_search?resource_id=" + dataset_id
+response = requests.get(url)
+data = response.json()
+records = data["result"]["records"]
+df = pd.DataFrame(records)
+df["year"] = pd.to_numeric(df["year"])
+df["no_of_participants"] = pd.to_numeric(df["no_of_participants"])
+
+pivot_df = df.pivot(index="year", columns="course_category", values="no_of_participants")
+labels = [str(int(year)) for year in pivot_df.index]
+ax = pivot_df.plot(kind="line", marker="x", figsize=(12, 6))
+
+plt.title("PA Course Participation (2014 - 2016)")
+plt.xlabel("Year")
+plt.ylabel("Number of participants")
+plt.grid(True)
+plt.legend(title="Course Category")
+plt.xticks(ticks=pivot_df.index, labels=labels)
+plt.tight_layout()
+plt.style.use("fivethirtyeight")
+plt.savefig("C:/OtherProjects/Data-Analysis-Projects/Social/PA Course Participation (2014 - 2016)/Index.png")
+plt.show()
